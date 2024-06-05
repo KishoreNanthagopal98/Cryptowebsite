3cf9670b513776ea928f6f49f2ab9bd8fa685665
unknown <kishorenpn6@gmail.com>
2024-06-05 21:56:08 +0530

    Navbar changes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,28 +2,28 @@ import { Link } from "react-router-dom";
 import "./navbar.scss";
 
 function Navbar() {
-  return (
-    <>
-      <nav className="navBar">
+	return (
+		<>
+			<nav className="navBar">
 				<div className="navbarMainDiv">
 					<Link to="/">
-						<h2>Crypto</h2>
+						<p>Crypto</p>
 					</Link>
 					<ul className="navList">
 						<li>
-							<a href = "#home">Home</a>
+							<a href="#home">Home</a>
 						</li>
 						<li>
-							<a href = "#market">Market</a>
+							<a href="#market">Market</a>
 						</li>
 						<li>
-							<a href = "#joinus">Join Us</a>
+							<a href="#joinus">Join Us</a>
 						</li>
 					</ul>
 				</div>
-      </nav>
-    </>
-  );
+			</nav>
+		</>
+	);
 }
 
 export default Navbar;
