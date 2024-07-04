978407bb49c75e804bc5f64ccd0b7aa1136ef4ea
KishoreNanthagopal98 <kishore.nanthagopal@justdial.com>
2024-07-04 14:36:53 +0530

    Score card changes

--- src/components/GamesComponent/ScoreSection/ScoreSection.js
+++ src/components/GamesComponent/ScoreSection/ScoreSection.js
@@ -1,19 +1,21 @@
 import "../ScoreSection/scoresection.scss";
+import crackergif from "../../../assets/firecracker-emoji-celebration.gif";
 
-function ScoreSection({score, handleModal}) {
-  return ( 
-		<>
-			<div class="backdrop"></div>
-			<div className="gameover-modal">
-				<h2 className="gameover-title">Game Over!!</h2>
-				<p className="gameover-score gameover-title">Your Score is: {score}</p>
-				<br />
-				<button className="closebtn" onClick={handleModal}>
-          Close
-        </button>
-			</div>
-		</>
-	);
+function ScoreSection({ score, handleModal }) {
+  return (
+    <>
+      <div class="backdrop"></div>
+      <div className="gameover-modal">
+        <h2 className="gameover-title">Game Over!!</h2>
+        <p className="gameover-score gameover-title">Your Score is: {score}</p>
+				<img src={crackergif} className="cracker-gif"/>
+        <img src={crackergif} className="cracker-gif-right"/>
+        <br />
+        <button className="closebtn" onClick={handleModal}>Close</button>
+				
+      </div>
+    </>
+  );
 }
 
 export default ScoreSection;
