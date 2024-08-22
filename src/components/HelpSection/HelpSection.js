18fa2c82660d687c4aaa3a5a22a2b351def31bb1
KishoreNanthagopal98 <kishore.nanthagopal@justdial.com>
2024-08-22 12:59:23 +0530

    Form integration for reach me section

--- src/components/HelpSection/HelpSection.js
+++ src/components/HelpSection/HelpSection.js
@@ -6,11 +6,24 @@ import * as yup from "yup";
 import "./helpsection.scss";
 import TitleSection from "../titleSection/TitleSection";
 
-const schema = yup.object().shape({
-  name: yup.string().required("Name is required*").matches(/^.{3,255}$/, "Name should be minimum 3 characters.").matches(/^[a-zA-Z][a-zA-Z\-\ \.]{2,}$/i, "Name should have alphabets only."),
-  mobileNumber: yup.string().required("Phone Number is Required*").matches(/^[0-9]{10}$/, "Mobile No. should be 10 digits NUMBER."),
-  comment: yup.string().required("Message is required*").min(3).max(300),
-}).required();
+const schema = yup
+  .object()
+  .shape({
+    name: yup
+      .string()
+      .required("Name is required*")
+      .matches(/^.{3,255}$/, "Name should be minimum 3 characters.")
+      .matches(
+        /^[a-zA-Z][a-zA-Z\-\ \.]{2,}$/i,
+        "Name should have alphabets only."
+      ),
+    mobileNumber: yup
+      .string()
+      .required("Phone Number is Required*")
+      .matches(/^[0-9]{10}$/, "Mobile No. should be 10 digits NUMBER."),
+    comment: yup.string().required("Message is required*").min(3).max(300),
+  })
+  .required();
 
 function HelpSection() {
   const [formStatus, setFormStatus] = useState(false);
@@ -46,12 +59,12 @@ function HelpSection() {
 
   return (
     <div className="helpusform">
-      <div>
+      <div className = "titleDiv">
         <TitleSection title="Reach out to me" />
       </div>
       <div className="formSection">
-        <div className={!formStatus ? "formNotSubmit" : "formSubmit"}>
-          <div className="formSt">
+        <div className="formSt">
+          <div className={formStatus ? "formFailure" : "formSuccess"}>
             <div className="formfields">
               <form className="formDiv" onSubmit={handleSubmit(onSubmit)}>
                 <div className="fields">
@@ -82,7 +95,7 @@ function HelpSection() {
                 </div>
                 <br />
                 <div className="fields">
-                  <label>Message / Concern</label>
+                  <label>Message/Concern</label>
                   <textarea
                     name="description"
                     rows="5"
@@ -102,8 +115,32 @@ function HelpSection() {
               </form>
             </div>
           </div>
+          <div className={formStatus ? "formSuccess" : "formFailure"}>
+            <div className="successMessage">
+              <div className="messageCont">
+                <svg className="successSvg" viewBox="0 0 24 24">
+                  <path
+                    fill="white"
+                    d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4M11,16.5L6.5,12L7.91,10.59L11,13.67L16.59,8.09L18,9.5L11,16.5Z"
+                  ></path>
+                </svg>
+                <br />
+                <br />
+                <br />
+
+                <div className="messageLine">
+                  <h4 className="submitLineOne">
+                    Thank you for connecting with me !!
+                  </h4>
+                  <br />
+                  <h2 className="submitLineTwo">
+                    I will get in touch with you shortly
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={formStatus ? "formNotSubmit" : "formSubmit"}></div>
       </div>
     </div>
   );
